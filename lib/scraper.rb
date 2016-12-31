705df16f5812a48c0d6eac8701fd645aeb371dd0
Michael Burak <michaelrburak@gmail.com>
2016-12-31 23:33:25 +0000

    scraper now scrapes pages and orders podcasts from PEL

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,21 +1,28 @@
-require 'open-uri' #this is a gem that lets you open up a page from the web for nokogiri to examine
-require 'pry' #this is a debugging tool
-require 'nokogiri' #this is what scrapes the data from the website
+require 'open-uri'
+require 'pry'
+require 'nokogiri'
 
-class Scraper #creative, huh?
+class Scraper
 
-  def scrape_podcast_page #gets the data from the page
-      Nokogiri::HTML(open('http://philosophizethis.org/category/episode/'))
+  def scrape_podcast_page
+      Nokogiri::HTML(open('http://www.partiallyexaminedlife.com/category/podcast-episodes/?order=ASC'))
     end
 
-  def scrape_podcast_index #uses the CSS(styling) of the page to locate the title section of the podcast
-    self.scrape_podcast_page.xpath("//h2[@class='alpha entry-title']")
+  def scrape_podcast_index
+    self.scrape_podcast_page.xpath("//article/header[@class='entry-header']/h2[@class='entry-title']")
   end
 
-  def order #puts out the text portion - ie. the title - of the podcast
-    self.scrape_podcast_index.each do |ti| puts ti.text
+  def order
+    counter = 0
+    self.scrape_podcast_index.each do |ti|
+    counter += 1
+    if ti.text.include? "PREVIEW" then nil
+    elsif ti.text.include? "Citizens Only" then nil
+    elsif ti.text.include? "Citizen Edition" then nil
+    else puts "#{counter}: #{ti.text}"
     end
+  end
 end
 
 end
-  binding.pry #allows for mucking about in the code
+  binding.pry
