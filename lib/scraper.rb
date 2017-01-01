268cd2ac8877d758416887b86ac5494f3cff8049
Michael Burak <michaelrburak@gmail.com>
2017-01-01 04:22:50 +0000

    added get_tag method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,15 +4,15 @@ require 'nokogiri'
 
 class Scraper
 
-  def scrape_podcast_page
+  def scrape_podcast_page #grabs the overall page of podcasts
       Nokogiri::HTML(open('http://www.partiallyexaminedlife.com/category/podcast-episodes/?order=ASC'))
     end
 
-  def scrape_podcast_index
+  def scrape_podcast_index #grabs the names of the podcasts
     self.scrape_podcast_page.xpath("//article/header[@class='entry-header']/h2[@class='entry-title']")
   end
 
-  def order
+  def order #removes non-free episodes, makes a list of free episodes
     counter = 0
     self.scrape_podcast_index.each do |ti|
     counter += 1
@@ -24,5 +24,14 @@ class Scraper
   end
 end
 
+  def get_tags #gets tags from inside article class, doesn't support more than one word tags
+        self.scrape_podcast_page.xpath("//article").each_with_index do |tag_set,i|
+      puts "#{i} #{tag_set.to_s.scan(/(tag-)(\w+)/)} "
+    end
+  end
+
 end
   binding.pry
+
+#find tags - attr('class').value
+#regex for tags in strings? (tag-)(\w+)
